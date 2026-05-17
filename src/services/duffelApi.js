99af28f4000b7fa400ea85c1ed905e629b56189d
John Kingoo <kingoojohn27@gmail.com>
2026-05-17 11:03:37 +0300

    Point frontend to live backend

--- src/services/duffelApi.js
+++ src/services/duffelApi.js
@@ -1,9 +1,9 @@
-import axios from "axios";
+const response = await axios.post(`${import.meta.env.VITE_API_URL}/flights`, {const response = await axios.post(`${import.meta.env.VITE_API_URL}/flights`, {import axios from "axios";
 
 export async function searchFlights({ origin, destination, date, passengers }) {
   console.log("CALLING BACKEND:", { origin, destination, date, passengers });
 
-  const response = await axios.post("http://localhost:3001/flights", {
+  const response = await axios.post(`${import.meta.env.VITE_API_URL}/flights`, {
     origin,
     destination,
     date,
--- src/services/hotelsApi.js
+++ src/services/hotelsApi.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export async function searchHotels({ city, checkIn, checkOut, adults = 1 }) {
-  const response = await axios.get("http://localhost:3001/hotels", {
+ const response = await axios.get(`${import.meta.env.VITE_API_URL}/hotels`, {
     params: { city, checkIn, checkOut, adults },
   });
   return response.data;
