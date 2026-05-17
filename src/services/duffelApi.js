6512485c104198b602cd6a2545100d87bb3c54ab
John Kingoo <kingoojohn27@gmail.com>
2026-05-17 11:28:53 +0300

    Fix duppleApi template literal

--- src/services/duffelApi.js
+++ src/services/duffelApi.js
@@ -1,4 +1,4 @@
-const response = await axios.post(`${import.meta.env.VITE_API_URL}/flights`, {const response = await axios.post(`${import.meta.env.VITE_API_URL}/flights`, {import axios from "axios";
+import axios from "axios";
 
 export async function searchFlights({ origin, destination, date, passengers }) {
   console.log("CALLING BACKEND:", { origin, destination, date, passengers });
