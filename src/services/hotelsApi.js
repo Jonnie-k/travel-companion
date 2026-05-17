b4f97b861b0d0827485b989b111e0b8fc4723bde
John Kingoo <kingoojohn27@gmail.com>
2026-05-17 11:35:43 +0300

    Fix API service URLs

--- src/services/hotelsApi.js
+++ src/services/hotelsApi.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-export async function searchHotels({ city, checkIn, checkOut, adults = 1 }) {
- const response = await axios.get(`${import.meta.env.VITE_API_URL}/hotels`, {
+export async function searchHotels({ city, checkIn, checkOut, adults }) {
+  const response = await axios.get(`${import.meta.env.VITE_API_URL}/hotels`, {
     params: { city, checkIn, checkOut, adults },
   });
   return response.data;
